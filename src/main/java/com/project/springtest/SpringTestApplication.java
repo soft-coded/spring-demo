c6ff821574b152839ef035118a68f25fdf5c1805
shrutanten <shrutanten.work@gmail.com>
2023-09-22 15:28:42 +0530

    Commit

--- src/main/java/com/project/springtest/SpringTestApplication.java
+++ src/main/java/com/project/springtest/SpringTestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
